53e8876929c3b82f5d642cec5d874e1b2c54a789
ethandhunt <46333321+ethandhunt@users.noreply.github.com>
2021-01-30 12:15:14 +1300

    added basic update program

---
+++ basicupdate.py
@@ -0,0 +1,17 @@
+import urllib.request
+
+def update(filestr):
+    try:
+        fp = urllib.request.urlopen(f"https://raw.githubusercontent.com/ethandhunt/Chatroom/main/{filestr}")
+        mybytes = fp.read()
+        string = mybytes.decode("utf8")
+        fp.close()
+        
+        file = open(filestr, "w")
+        file.write(string)
+        file.close()
+        print(f"[{filestr}] Update Complete")
+    except urllib.error.HTTPError:
+        print(f"[{filestr}] That File Doesn't Seem To Exist")
+
+update("")
