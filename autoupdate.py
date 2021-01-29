62afd1a7ed1cc8c7cb42916bc1d67992b35d21d9
ethandhunt <46333321+ethandhunt@users.noreply.github.com>
2021-01-29 17:36:14 +1300

    Made An Auto Updater
    Gets files from the github repo and saves them

---
+++ autoupdate.py
@@ -0,0 +1,19 @@
+import urllib.request
+
+def update(filestr):
+    try:
+        fp = urllib.request.urlopen(f"https://raw.githubusercontent.com/ethandhunt/Chatroom/main/{filestr}")
+        mybytes = fp.read()
+        string = mybytes.decode("utf8")
+        fp.close()
+        file = open(filestr, "w")
+        file.write(str("".join(string.split("\n"))))
+        file.close()
+        print(f"Updated {filestr}")
+    except urllib.error.HTTPError:
+        print("That File Doesn't Seem To Exist")
+
+update("webclient.py")
+update("webserver.py")
+update("README.md")
+update("autoupdate.py")
